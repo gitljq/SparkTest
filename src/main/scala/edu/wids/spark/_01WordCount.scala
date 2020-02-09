17a771c7eb43bb6a5001faea24f60b8814a79566
ljq <1366404872@qq.com>
2020-02-09 18:15:51 +0800

    Spark学习过程

---
+++ src/main/scala/edu/wids/spark/_01WordCount.scala
@@ -0,0 +1,11 @@
+package edu.wids.spark
+
+import org.apache.spark.SparkContext
+
+/**
+  * Created by Administrator on 2020/2/8 21:46
+  */
+class _01WordCount {
+  private val sparkContext: SparkContext = new SparkContext()
+
+}
